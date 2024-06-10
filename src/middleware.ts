18b1f25598abf6346b20cf6d242c112ce726a859
Akhilesh Upadhyay <umapati4381@gmail.com>
2024-06-10 14:35:10 +0530

    learn middleware.js

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // middleware will run before accessing a page.js / route or requesting to a route handler
+
+  /* cookies,headers,send response ,NextResponse.next() : return early,CORS*/
+  return NextResponse.json({ msg: "hello from middleware" });
+
+  return NextResponse.rewrite(
+    new URL("/streamUi", request.url)
+  ); /* similar to redirect , but preserves url on browser */
+
+  return NextResponse.redirect(
+    new URL("/streamUi", request.url)
+  ); /* always redirect '/' to '/streamUi' */
+}
+
+export const config = {
+  matcher: "/" /* cant use variables here ,read docs */,
+};
